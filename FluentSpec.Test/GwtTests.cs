1c9216c993c5e8835c4ac9979757a831ae9a9f0d
Ke Zhu <kzhu@onetechnologies.net>
2011-05-26 22:55:58 -0500

    shit aint working

---
+++ FluentSpec.Test/GwtTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NSpec;
+using NUnit.Framework;
+
+namespace FluentSpec.Test
+{
+    [TestFixture]
+    public class BankKata
+    {
+        [Test]
+        public void withdraw_test()
+        {
+            var spec = new Gwt<Account>();
+            spec.Given(account => new Account())
+                .When("Has 50 bucks", account => account.Balance = 50M)
+                .Then("Should be able to withdraw 30 dollar", account => account.CanWithdraw(30M).should_be_true());
+        }
+    }
+
+    public class Account
+    {
+        public decimal Balance { get; set; }
+        public bool CanWithdraw(decimal amount)
+        {
+            return Balance >= amount;
+        }
+    }
+
+}
---
+++ FluentSpec/Gwt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NSpec;
+
+namespace FluentSpec
+{
+    public class Gwt<TContext> : nspec where TContext : class
+    {
+        public TContext subject { get; set; }
+        public Gwt<TContext> Given(Action<TContext> initializer)
+        {
+            before = () => initializer(subject);
+            return this;
+        }
+
+        public Gwt<TContext> When(string contextDescription, Action<TContext> whenAction)
+        {
+            context[contextDescription] = () => whenAction(subject);
+            return this;
+        }
+
+        public Gwt<TContext> Then(string contextDescription, Action<TContext> thenAction)
+        {
+            it[contextDescription] = () => thenAction(subject);
+            return this;
+        }
+
+
+    }
+}
